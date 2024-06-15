2a1ef61e748671995a5e3d0fbc2d450b33c838a3
Patrick John Olivar <138101559+NTC-ashrah@users.noreply.github.com>
2024-06-15 16:51:29 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract Assessment {
     address payable public owner;
     uint256 public balance;
-
+    uint256 public tokenBalance;
+    
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
 
@@ -17,21 +18,21 @@ contract Assessment {
 
     function getBalance() public view returns(uint256){
         return balance;
-    }
+    }   
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
 
-        // make sure this is the owner
+        // validate sender
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
+        // execute transaction
         balance += _amount;
 
-        // assert transaction completed successfully
+        // assert transaction integrity
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
+        // emit event
         emit Deposit(_amount);
     }
 
@@ -48,13 +49,29 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
+        // execute withdrawal
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
+        // assert balance integrity
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
+        // emit event
         emit Withdraw(_withdrawAmount);
     }
+
+    function convertToToken(uint256 _amount) public {
+
+        require(balance >= _amount, "You don't have enough balance.");
+
+        tokenBalance = _amount;
+        balance -= _amount;
+        
+    }
+
+    function getTokens() public view returns (uint256) {
+
+        return tokenBalance;
+        
+    }
+
 }
